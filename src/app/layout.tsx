2f3ade728a378108ccd9b0b573d7445d2462b50d
vilisimo <vilisimo@gmail.com>
2023-10-30 20:52:43 +0200

    Enable Tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import React from 'react'
 
+import './globals.css'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -14,6 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
+        <h1 className="text-center text-3xl font-extrabold m-8">Supercharge your Monefy data</h1>
         <MonefyUploader>{children}</MonefyUploader>
       </body>
     </html>
